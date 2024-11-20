16a3766385fcd4f17719254f7715185aee0b7711
Shahrukh Khan <61946634+shahrukh263@users.noreply.github.com>
2024-11-21 01:28:47 +0530

    new comp added

---
+++ components/PlatformComp.tsx
@@ -0,0 +1,8 @@
+import { NextComponentType } from "next";
+
+const PlatformComt: NextComponentType =()=>{
+    return(
+        <></>
+    );
+}
+export default PlatformComt;
--- pages/social-inbox.js
+++ pages/social-inbox.js
@@ -1,27 +1,30 @@
-import Head from 'next/head';
+import Head from "next/head";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from 'react';
-import SocialHero from '../components/SocialHero';
-import BrandComp from '../components/BrandComp';
-import Footer from '../components/footer';
+import { useEffect } from "react";
+import SocialHero from "../components/SocialHero";
+import BrandComp from "../components/BrandComp";
+import Footer from "../components/footer";
+import PlatformComt from "../components/PlatformComp";
 const SocialInbox = () => {
-	useEffect(() => {
-        AOS.init();
-      }, [])
-	return (
-		<>
-			<Head>
-				<title>Social Inbox</title>
-			</Head>
-            <SocialHero/>
-            <BrandComp/>
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <>
+      <Head>
+        <title>Social Inbox</title>
+      </Head>
+      <SocialHero />
+      <BrandComp />
+      <PlatformComt />
 
 
 
-            <Footer/>
-		</>
-	);
+
+      <Footer />
+    </>
+  );
 };
 
 export default SocialInbox;
